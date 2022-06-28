24975158170c56fc54ac1a48fbcf8d1742d8e741
BaeByoungSul <xyz5787@naver.com>
2022-06-28 22:43:11 +0900

    test

--- WcfDBClient/frmMySQL.Designer.cs
+++ WcfDBClient/frmMySQL.Designer.cs
@@ -44,54 +44,61 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 30);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(46, 40);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 15);
+            this.label1.Size = new System.Drawing.Size(68, 20);
             this.label1.TabIndex = 17;
             this.label1.Text = "Bindings";
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(341, 76);
+            this.btnClear.Location = new System.Drawing.Point(438, 101);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(114, 46);
+            this.btnClear.Size = new System.Drawing.Size(147, 61);
             this.btnClear.TabIndex = 16;
             this.btnClear.Text = "Clear";
             this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // btn_ins2
             // 
-            this.btn_ins2.Location = new System.Drawing.Point(658, 76);
+            this.btn_ins2.Location = new System.Drawing.Point(846, 101);
+            this.btn_ins2.Margin = new System.Windows.Forms.Padding(4);
             this.btn_ins2.Name = "btn_ins2";
-            this.btn_ins2.Size = new System.Drawing.Size(107, 46);
+            this.btn_ins2.Size = new System.Drawing.Size(138, 61);
             this.btn_ins2.TabIndex = 15;
             this.btn_ins2.Text = "등록(DataSet)";
             this.btn_ins2.UseVisualStyleBackColor = true;
             // 
             // btn_ins1
             // 
-            this.btn_ins1.Location = new System.Drawing.Point(538, 76);
+            this.btn_ins1.Location = new System.Drawing.Point(692, 101);
+            this.btn_ins1.Margin = new System.Windows.Forms.Padding(4);
             this.btn_ins1.Name = "btn_ins1";
-            this.btn_ins1.Size = new System.Drawing.Size(114, 46);
+            this.btn_ins1.Size = new System.Drawing.Size(147, 61);
             this.btn_ins1.TabIndex = 14;
             this.btn_ins1.Text = "등록(List)";
             this.btn_ins1.UseVisualStyleBackColor = true;
+            this.btn_ins1.Click += new System.EventHandler(this.btn_ins1_Click);
             // 
             // btn_sel_2
             // 
-            this.btn_sel_2.Location = new System.Drawing.Point(156, 76);
+            this.btn_sel_2.Location = new System.Drawing.Point(201, 101);
+            this.btn_sel_2.Margin = new System.Windows.Forms.Padding(4);
             this.btn_sel_2.Name = "btn_sel_2";
-            this.btn_sel_2.Size = new System.Drawing.Size(107, 46);
+            this.btn_sel_2.Size = new System.Drawing.Size(138, 61);
             this.btn_sel_2.TabIndex = 13;
             this.btn_sel_2.Text = "조회(DataSet)";
             this.btn_sel_2.UseVisualStyleBackColor = true;
+            this.btn_sel_2.Click += new System.EventHandler(this.btn_sel_2_Click);
             // 
             // btn_sel_1
             // 
-            this.btn_sel_1.Location = new System.Drawing.Point(36, 76);
+            this.btn_sel_1.Location = new System.Drawing.Point(46, 101);
+            this.btn_sel_1.Margin = new System.Windows.Forms.Padding(4);
             this.btn_sel_1.Name = "btn_sel_1";
-            this.btn_sel_1.Size = new System.Drawing.Size(114, 46);
+            this.btn_sel_1.Size = new System.Drawing.Size(147, 61);
             this.btn_sel_1.TabIndex = 12;
             this.btn_sel_1.Text = "조회(List)";
             this.btn_sel_1.UseVisualStyleBackColor = true;
@@ -100,21 +107,23 @@
             // dgv2
             // 
             this.dgv2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv2.Location = new System.Drawing.Point(424, 135);
+            this.dgv2.Location = new System.Drawing.Point(545, 180);
+            this.dgv2.Margin = new System.Windows.Forms.Padding(4);
             this.dgv2.Name = "dgv2";
             this.dgv2.RowHeadersWidth = 51;
             this.dgv2.RowTemplate.Height = 25;
-            this.dgv2.Size = new System.Drawing.Size(341, 285);
+            this.dgv2.Size = new System.Drawing.Size(438, 380);
             this.dgv2.TabIndex = 11;
             // 
             // dgv1
             // 
             this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv1.Location = new System.Drawing.Point(36, 135);
+            this.dgv1.Location = new System.Drawing.Point(46, 180);
+            this.dgv1.Margin = new System.Windows.Forms.Padding(4);
             this.dgv1.Name = "dgv1";
             this.dgv1.RowHeadersWidth = 51;
             this.dgv1.RowTemplate.Height = 25;
-            this.dgv1.Size = new System.Drawing.Size(341, 285);
+            this.dgv1.Size = new System.Drawing.Size(438, 380);
             this.dgv1.TabIndex = 10;
             // 
             // cbo1
@@ -124,16 +133,17 @@
             this.cbo1.Items.AddRange(new object[] {
             "Http",
             "NetTcp"});
-            this.cbo1.Location = new System.Drawing.Point(36, 47);
+            this.cbo1.Location = new System.Drawing.Point(46, 63);
+            this.cbo1.Margin = new System.Windows.Forms.Padding(4);
             this.cbo1.Name = "cbo1";
-            this.cbo1.Size = new System.Drawing.Size(121, 23);
+            this.cbo1.Size = new System.Drawing.Size(154, 28);
             this.cbo1.TabIndex = 9;
             // 
             // frmMySQL
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1029, 600);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btn_ins2);
@@ -143,6 +153,7 @@
             this.Controls.Add(this.dgv2);
             this.Controls.Add(this.dgv1);
             this.Controls.Add(this.cbo1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmMySQL";
             this.Text = "frmMySQL";
             ((System.ComponentModel.ISupportInitialize)(this.dgv2)).EndInit();
--- WcfDBClient/frmMySQL.cs
+++ WcfDBClient/frmMySQL.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace WcfDBClient
 {
@@ -50,8 +52,8 @@ namespace WcfDBClient
 
 
             _cmd.Parameters = new MyPara[2];
-            _cmd.Parameters[0] = new MyPara("P_TEST_MST_NM", (int)SqlDbType.NVarChar, (int)ParameterDirection.Input);
-            _cmd.Parameters[1] = new MyPara("P_TEST_PARA", (int)SqlDbType.NVarChar, (int)ParameterDirection.Input);
+            _cmd.Parameters[0] = new MyPara("P_TEST_MST_NM", (int)MySqlDbType.VarChar, (int)ParameterDirection.Input);
+            _cmd.Parameters[1] = new MyPara("P_TEST_PARA", (int)MySqlDbType.VarChar, (int)ParameterDirection.Input);
 
             //가변배열 초기화
             MyParaValue[][] myParaValues = new MyParaValue[1][];
@@ -63,6 +65,151 @@ namespace WcfDBClient
             _cmd.ParaValues = myParaValues;
             return _cmd;
         }
+
+        private void btn_sel_2_Click(object sender, EventArgs e)
+        {
+            // 기본 binding
+            MyBindinEnum bindinEnum = MyBindinEnum.Http;
+
+            if (cbo1.Text.Equals("Http")) bindinEnum = MyBindinEnum.Http;
+            else if (cbo1.Text.Equals("NetTcp")) bindinEnum = MyBindinEnum.NetTcp;
+
+            DBClient _cli = new DBClient(bindinEnum);
+            SvcReturnDs rtn = _cli.GetDataSet(GetCmd());
+
+            dgv1.DataSource = null;
+            dgv2.DataSource = null;
+
+            if (rtn.ReturnCD == "OK")
+            {
+                dgv1.DataSource = rtn.ReturnDs?.Tables[0];
+                dgv2.DataSource = rtn.ReturnDs?.Tables[1];
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            dgv1.DataSource = null;
+            dgv2.DataSource = null;
+        }
+
+        private void btn_ins1_Click(object sender, EventArgs e)
+        {
+            //
+            StringBuilder strQuery1 = new StringBuilder();
+            strQuery1.AppendLine("INSERT INTO  TESTDB..A_TEST1(COL1,COL2) VALUES ('dfd','222' ) ");
+            strQuery1.AppendLine("INSERT INTO  TESTDB..A_TEST21(COL1,COL2) VALUES ('df','222' ) ");
+            strQuery1.AppendLine("INSERT INTO  TESTDB..A_TEST1(COL1,COL2) VALUES ('fdf','222' ) ");
+
+            // Create Db Command
+            List<MyCommand> mycmds = new List<MyCommand>();
+            MyCommand cmd = new MyCommand("MST_1", "BSBAE", (int)CommandType.Text, strQuery1.ToString());
+
+            MyCommand cmdMst = ITEM_MST_Command();
+            MyCommand cmdDtl = ITEM_DTL_Command();
+            mycmds.AddRange(new MyCommand[] { cmdMst, cmdDtl });
+
+            try
+            {
+                Cursor = Cursors.WaitCursor;
+
+                if (cbo1.Text == String.Empty)
+                {
+                    MessageBox.Show("Select Binding");
+                    return;
+                }
+
+                // 기본 binding
+                MyBindinEnum bindinEnum = MyBindinEnum.Http;
+
+                if (cbo1.Text.Equals("Http")) bindinEnum = MyBindinEnum.Http;
+                else if (cbo1.Text.Equals("NetTcp")) bindinEnum = MyBindinEnum.NetTcp;
+
+                DBClient _cli = new DBClient(bindinEnum);
+                //_cli.SetTransOption(System.Transactions.TransactionScopeOption.Suppress);
+
+                SvcReturnList<DBOutPut> rtn = _cli.ExecQuery<DBOutPut>(mycmds);
+
+                if (rtn.ReturnCD.Equals("OK"))
+                {
+                    dgv1.DataSource = rtn.ReturnList;
+                }
+                else
+                {
+                    MessageBox.Show(rtn.ReturnMsg);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
+            }
+        }
+
+        private MyCommand ITEM_MST_Command()
+        {
+
+            MyCommand _cmd = new MyCommand("MST", "MYSQL_DB",
+                (int)CommandType.StoredProcedure, "USP_TEST_MST_INS");
+
+            _cmd.Parameters = new MyPara[2];
+            _cmd.Parameters[0] = new MyPara("P_TEST_MST_NM", (int)MySqlDbType.VarChar, (int)ParameterDirection.Input);
+            _cmd.Parameters[1] = new MyPara("P_TEST_ID", (int)MySqlDbType.UInt64, (int)ParameterDirection.Output);
+
+            // Parameter Value 가변배열 초기화
+            MyParaValue[][] myParaValues = new MyParaValue[1][]; // 한세트 정의
+            myParaValues[0] = new MyParaValue[2];
+
+            myParaValues[0][0] = new MyParaValue("P_TEST_MST_NM", "배병술");
+            myParaValues[0][1] = new MyParaValue("P_TEST_ID", "");
+
+            _cmd.ParaValues = myParaValues;
+
+            return _cmd;
+        }
+        private MyCommand ITEM_DTL_Command()
+        {
+            MyCommand _cmd = new MyCommand("DTL", "MYSQL_DB",
+                            (int)CommandType.StoredProcedure, "USP_TEST_DTL_INS");
+
+
+            _cmd.Parameters = new MyPara[3];
+
+            _cmd.Parameters[0] = new MyPara("P_TEST_ID", (int)MySqlDbType.UInt64, (int)ParameterDirection.Input,
+                "MST", "@TEST_ID");
+            _cmd.Parameters[1] = new MyPara("P_TEST_DTL_NM", (int)MySqlDbType.VarChar, (int)ParameterDirection.Input);
+            _cmd.Parameters[2] = new MyPara("P_AMOUNT", (int)MySqlDbType.Decimal, (int)ParameterDirection.Input);
+
+
+
+            // Parameter Value 가변배열 초기화
+            MyParaValue[][] myParaValues = new MyParaValue[3][]; // 3세트 정의
+
+            myParaValues[0] = new MyParaValue[3]; // 첫번째 셋트
+            myParaValues[0][0] = new MyParaValue("P_TEST_ID", string.Empty);
+            myParaValues[0][1] = new MyParaValue("P_TEST_DTL_NM", "놀부1");
+            myParaValues[0][2] = new MyParaValue("P_AMOUNT", "123.5");
+
+            myParaValues[1] = new MyParaValue[3]; // 두번째 셋트
+            myParaValues[1][0] = new MyParaValue("P_TEST_ID", string.Empty);
+            myParaValues[1][1] = new MyParaValue("P_TEST_DTL_NM", "놀부2");
+            myParaValues[1][2] = new MyParaValue("P_AMOUNT", "222.5");
+
+            myParaValues[2] = new MyParaValue[3]; // 세번째 셋트
+            myParaValues[2][0] = new MyParaValue("P_TEST_ID", string.Empty);
+            myParaValues[2][1] = new MyParaValue("P_TEST_DTL_NM", "흥부");
+            myParaValues[2][2] = new MyParaValue("P_AMOUNT", "333.5");
+
+            _cmd.ParaValues = myParaValues;
+
+            return _cmd;
+        }
+
 
     }
 }
